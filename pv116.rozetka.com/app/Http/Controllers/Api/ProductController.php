8046ece981ceb1c21f248b24902648d32e765c67
dimonkiss <dklxwa@gmail.com>
2024-01-12 20:37:42 +0200

    add multiple images

--- pv116.rozetka.com/app/Http/Controllers/Api/ProductController.php
+++ pv116.rozetka.com/app/Http/Controllers/Api/ProductController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Categories;
 use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
-use Intervention\Image\Drivers\Gd\Driver;
-use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -20,20 +19,20 @@ class ProductController extends Controller
      */
     public function getList()
     {
-//        $data = Product::all();
-        $data = Product::with('category')->get();
+        $data = Product::with('category', 'images')->get();
         return response()->json($data)
             ->header('Content-Type', 'application/json; charset=utf-8');
     }
+
     /**
      * @OA\Post(
      *     tags={"Product"},
      *     path="/api/products",
      *     @OA\RequestBody(
      *         @OA\MediaType(
-     *             mediaType="application/json",
+     *             mediaType="multipart/form-data",
      *             @OA\Schema(
-     *                 required={"name", "description", "price", "quantity", "category_id"},
+     *                 required={"name", "description", "price", "quantity", "category_id", "images"},
      *                 @OA\Property(
      *                     property="name",
      *                     type="string"
@@ -53,6 +52,13 @@ class ProductController extends Controller
      *                 @OA\Property(
      *                     property="category_id",
      *                     type="integer"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="images",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="file"
+     *                     )
      *                 )
      *             )
      *         )
@@ -60,13 +66,48 @@ class ProductController extends Controller
      *     @OA\Response(response="201", description="Product created.")
      * )
      */
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+            'category_id' => 'required|integer',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
         $input = $request->all();
-        $product = Product::create($input);
+
+        // Create the product
+        $product = Product::create([
+            'name' => $input['name'],
+            'description' => $input['description'],
+            'price' => $input['price'],
+            'quantity' => $input['quantity'],
+            'category_id' => $input['category_id'],
+        ]);
+
+        // Upload and attach images
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+                $imageName = time() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'name' => $imageName,
+                ]);
+            }
+        }
 
         return response()->json($product, 201, [
             'Content-Type' => 'application/json;charset=UTF-8',
-            'Charset' => 'utf-8'
+            'Charset' => 'utf-8',
         ], JSON_UNESCAPED_UNICODE);
     }
 }
